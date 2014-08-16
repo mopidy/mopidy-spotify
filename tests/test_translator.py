57047e276a74dbf9d62dfc5e01af2ba0940b62b2
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-08-16 23:43:02 +0200

    Group translator tests by function they test

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -7,94 +7,92 @@ import spotify
 from mopidy_spotify import translator
 
 
-def test_to_track_returns_none_if_unloaded(sp_track_mock):
-    sp_track_mock.is_loaded = False
+class TestToTrack(object):
 
-    track = translator.to_track(sp_track_mock)
+    def test_to_track_returns_none_if_unloaded(self, sp_track_mock):
+        sp_track_mock.is_loaded = False
 
-    assert track is None
+        track = translator.to_track(sp_track_mock)
 
+        assert track is None
 
-def test_to_track_returns_none_if_error(sp_track_mock):
-    sp_track_mock.error = spotify.ErrorType.OTHER_PERMANENT
+    def test_to_track_returns_none_if_error(self, sp_track_mock):
+        sp_track_mock.error = spotify.ErrorType.OTHER_PERMANENT
 
-    track = translator.to_track(sp_track_mock)
+        track = translator.to_track(sp_track_mock)
 
-    assert track is None
+        assert track is None
 
+    def test_to_track_returns_none_if_not_available(self, sp_track_mock):
+        sp_track_mock.availability = spotify.TrackAvailability.UNAVAILABLE
 
-def test_to_track_returns_none_if_not_available(sp_track_mock):
-    sp_track_mock.availability = spotify.TrackAvailability.UNAVAILABLE
+        track = translator.to_track(sp_track_mock)
 
-    track = translator.to_track(sp_track_mock)
+        assert track is None
 
-    assert track is None
+    def test_to_track(self, sp_track_mock):
+        track = translator.to_track(sp_track_mock)
 
-
-def test_to_track(sp_track_mock):
-    track = translator.to_track(sp_track_mock)
-
-    assert track.uri == 'spotify:track:abc'
-    assert track.name == 'ABC 123'
-    assert track.length == 174300
-    assert track.track_no == 7
-
-
-def test_to_playlist_returns_none_if_unloaded():
-    sp_playlist = mock.Mock(spec=spotify.Playlist)
-    sp_playlist.is_loaded = False
-
-    playlist = translator.to_playlist(sp_playlist)
-
-    assert playlist is None
+        assert track.uri == 'spotify:track:abc'
+        assert track.name == 'ABC 123'
+        assert track.length == 174300
+        assert track.track_no == 7
 
 
-def test_to_playlist_returns_none_if_playlist_folder():
-    sp_playlist_folder = mock.Mock(spec=spotify.PlaylistFolder)
+class TestToPlaylist(object):
 
-    playlist = translator.to_playlist(sp_playlist_folder)
+    def test_to_playlist_returns_none_if_unloaded(self):
+        sp_playlist = mock.Mock(spec=spotify.Playlist)
+        sp_playlist.is_loaded = False
 
-    assert playlist is None
+        playlist = translator.to_playlist(sp_playlist)
 
+        assert playlist is None
 
-def test_to_playlist(sp_track_mock, sp_playlist_mock):
-    track = translator.to_track(sp_track_mock)
-    playlist = translator.to_playlist(sp_playlist_mock)
+    def test_to_playlist_returns_none_if_playlist_folder(self):
+        sp_playlist_folder = mock.Mock(spec=spotify.PlaylistFolder)
 
-    assert playlist.uri == 'spotify:playlist:alice:foo'
-    assert playlist.name == 'Foo'
-    assert playlist.length == 1
-    assert track in playlist.tracks
-    assert playlist.last_modified is None
+        playlist = translator.to_playlist(sp_playlist_folder)
 
+        assert playlist is None
 
-def test_to_playlist_adds_name_for_starred_playlists(sp_playlist_mock):
-    sp_playlist_mock.name = None
+    def test_to_playlist(self, sp_track_mock, sp_playlist_mock):
+        track = translator.to_track(sp_track_mock)
+        playlist = translator.to_playlist(sp_playlist_mock)
 
-    playlist = translator.to_playlist(sp_playlist_mock)
+        assert playlist.uri == 'spotify:playlist:alice:foo'
+        assert playlist.name == 'Foo'
+        assert playlist.length == 1
+        assert track in playlist.tracks
+        assert playlist.last_modified is None
 
-    assert playlist.name == 'Starred'
+    def test_to_playlist_adds_name_for_starred_playlists(
+            self, sp_playlist_mock):
+        sp_playlist_mock.name = None
 
+        playlist = translator.to_playlist(sp_playlist_mock)
 
-def test_to_playlist_filters_out_none_tracks(sp_track_mock, sp_playlist_mock):
-    sp_track_mock.is_loaded = False
-    playlist = translator.to_playlist(sp_playlist_mock)
+        assert playlist.name == 'Starred'
 
-    assert playlist.length == 0
-    assert list(playlist.tracks) == []
+    def test_to_playlist_filters_out_none_tracks(
+            self, sp_track_mock, sp_playlist_mock):
+        sp_track_mock.is_loaded = False
+        playlist = translator.to_playlist(sp_playlist_mock)
 
+        assert playlist.length == 0
+        assert list(playlist.tracks) == []
 
-def test_to_playlist_includes_by_owner_in_name_if_owned_by_another_user(
-        sp_playlist_mock, sp_user_mock):
-    sp_user_mock.canonical_name = 'bob'
-    sp_playlist_mock.user = sp_user_mock
+    def test_to_playlist_includes_by_owner_in_name_if_owned_by_another_user(
+            self, sp_playlist_mock, sp_user_mock):
+        sp_user_mock.canonical_name = 'bob'
+        sp_playlist_mock.user = sp_user_mock
 
-    playlist = translator.to_playlist(sp_playlist_mock, username='alice')
+        playlist = translator.to_playlist(sp_playlist_mock, username='alice')
 
-    assert playlist.name == 'Foo by bob'
+        assert playlist.name == 'Foo by bob'
 
+    def test_to_playlist_includes_folders_in_name(self, sp_playlist_mock):
+        playlist = translator.to_playlist(
+            sp_playlist_mock, folders=['Bar', 'Baz'])
 
-def test_to_playlist_includes_folders_in_name(sp_playlist_mock):
-    playlist = translator.to_playlist(sp_playlist_mock, folders=['Bar', 'Baz'])
-
-    assert playlist.name == 'Bar/Baz/Foo'
+        assert playlist.name == 'Bar/Baz/Foo'
