c8285520b9e6fba9ff0e93404633cf1b2916df6f
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-11-10 23:59:07 +0100

    library: Split lookup impl into multiple functions

--- mopidy_spotify/library.py
+++ mopidy_spotify/library.py
@@ -25,33 +25,46 @@ class SpotifyLibraryProvider(backend.LibraryProvider):
             return []
 
         if sp_link.type is spotify.LinkType.TRACK:
-            sp_track = sp_link.as_track()
-            sp_track.load()
-            return [
-                translator.to_track(sp_track, bitrate=self._backend.bitrate)]
+            return list(self._lookup_track(sp_link))
         elif sp_link.type is spotify.LinkType.ALBUM:
-            sp_album = sp_link.as_album()
-            sp_album_browser = sp_album.browse()
-            sp_album_browser.load()
-            return [
-                translator.to_track(sp_track, bitrate=self._backend.bitrate)
-                for sp_track in sp_album_browser.tracks]
+            return list(self._lookup_album(sp_link))
         elif sp_link.type is spotify.LinkType.ARTIST:
-            sp_artist = sp_link.as_artist()
-            sp_artist_browser = sp_artist.browse(
-                type=spotify.ArtistBrowserType.NO_TRACKS)
-            sp_artist_browser.load()
-            return [
-                translator.to_track(sp_track, bitrate=self._backend.bitrate)
-                for sp_album in sp_artist_browser.albums
-                for sp_track in sp_album.browse().load().tracks]
+            return list(self._lookup_artist(sp_link))
         elif sp_link.type is spotify.LinkType.PLAYLIST:
-            sp_playlist = sp_link.as_playlist()
-            sp_playlist.load()
-            return [
-                translator.to_track(sp_track, bitrate=self._backend.bitrate)
-                for sp_track in sp_playlist.tracks]
+            return list(self._lookup_playlist(sp_link))
         else:
             logger.info(
                 'Failed to lookup "%s": Cannot handle %r', uri, sp_link.type)
             return []
+
+    def _lookup_track(self, sp_link):
+        sp_track = sp_link.as_track()
+        sp_track.load()
+        yield translator.to_track(sp_track, bitrate=self._backend.bitrate)
+
+    def _lookup_album(self, sp_link):
+        sp_album = sp_link.as_album()
+        sp_album_browser = sp_album.browse()
+        sp_album_browser.load()
+        for sp_track in sp_album_browser.tracks:
+            yield translator.to_track(
+                sp_track, bitrate=self._backend.bitrate)
+
+    def _lookup_artist(self, sp_link):
+        sp_artist = sp_link.as_artist()
+        sp_artist_browser = sp_artist.browse(
+            type=spotify.ArtistBrowserType.NO_TRACKS)
+        sp_artist_browser.load()
+        for sp_album in sp_artist_browser.albums:
+            sp_album_browser = sp_album.browse()
+            sp_album_browser.load()
+            for sp_track in sp_album_browser.tracks:
+                yield translator.to_track(
+                    sp_track, bitrate=self._backend.bitrate)
+
+    def _lookup_playlist(self, sp_link):
+        sp_playlist = sp_link.as_playlist()
+        sp_playlist.load()
+        for sp_track in sp_playlist.tracks:
+            yield translator.to_track(
+                sp_track, bitrate=self._backend.bitrate)
