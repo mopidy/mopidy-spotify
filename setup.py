4d63971d7e03d3fef02025d4b14b96f302f8c8ca
Stein Magnus Jodal <stein.magnus@jodal.no>
2014-11-14 15:13:52 +0100

    Require unreleased pyspotify 2.0.0b4
    We now use pyspotify features added after 2.0.0b3.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'setuptools',
         'Mopidy >= 0.18',
         'Pykka >= 1.1',
-        'pyspotify >= 2.0.0b3',
+        'pyspotify >= 2.0.0b4',
     ],
     entry_points={
         'mopidy.ext': [
