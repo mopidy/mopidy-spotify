0f452141a51c3ea55819c43630485d697b6d4dff
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-03-22 13:08:15 +0100

    playlists: Add old default behavior
    This was removed from the PlaylistProvider superclass in Mopidy 1.0.

--- mopidy_spotify/playlists.py
+++ mopidy_spotify/playlists.py
@@ -1,9 +1,24 @@
 from __future__ import unicode_literals
 
+import copy
+
 from mopidy import backend
 
 
 class SpotifyPlaylistsProvider(backend.PlaylistsProvider):
+
+    def __init__(self, backend):
+        super(SpotifyPlaylistsProvider, self).__init__(backend)
+        self._playlists = []
+
+    @property
+    def playlists(self):
+        return copy.copy(self._playlists)
+
+    @playlists.setter
+    def playlists(self, playlists):
+        self._playlists = playlists
+
     def create(self, name):
         pass  # TODO
 
