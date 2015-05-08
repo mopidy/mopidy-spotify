1311d547a178c61a28abe90f79e83431b79c5bda
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-05-09 01:10:58 +0200

    Require pyspotify >= 2.0.0b5

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
         'setuptools',
         'Mopidy >= 1.0',
         'Pykka >= 1.1',
-        'pyspotify >= 2.0.0b4',
+        'pyspotify >= 2.0.0b5',
     ],
     entry_points={
         'mopidy.ext': [
