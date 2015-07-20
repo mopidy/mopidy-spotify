bf639c3b7565c37062f2d0f7539e2cb30bf43a46
Stein Magnus Jodal <stein.magnus@jodal.no>
2015-07-20 12:41:20 +0200

    search: Split impl out of library

--- mopidy_spotify/library.py
+++ mopidy_spotify/library.py
@@ -1,12 +1,10 @@
 from __future__ import unicode_literals
 
 import logging
-import urllib
 
-from mopidy import backend, models
+from mopidy import backend
 
-import spotify
-from mopidy_spotify import browse, distinct, images, lookup, translator, utils
+from mopidy_spotify import browse, distinct, images, lookup, search
 
 
 logger = logging.getLogger(__name__)
@@ -33,52 +31,7 @@ class SpotifyLibraryProvider(backend.LibraryProvider):
         return lookup.lookup(self._config, self._backend._session, uri)
 
     def search(self, query=None, uris=None, exact=False):
-        # TODO Respect `uris` argument
-        # TODO Support `exact` search
-
-        if query is None:
-            logger.debug('Ignored search without query')
-            return models.SearchResult(uri='spotify:search')
-
-        if 'uri' in query:
-            return self._search_by_uri(query)
-
-        sp_query = translator.sp_search_query(query)
-        if not sp_query:
-            logger.debug('Ignored search with empty query')
-            return models.SearchResult(uri='spotify:search')
-
-        uri = 'spotify:search:%s' % urllib.quote(sp_query.encode('utf-8'))
-        logger.info('Searching Spotify for: %s', sp_query)
-
-        if not self._backend._online.is_set():
-            logger.info('Spotify search aborted: Spotify is offline')
-            return models.SearchResult(uri=uri)
-
-        sp_search = self._backend._session.search(
-            sp_query,
-            album_count=self._config['search_album_count'],
-            artist_count=self._config['search_artist_count'],
-            track_count=self._config['search_track_count'])
-        sp_search.load()
-
-        albums = [
-            translator.to_album(sp_album) for sp_album in sp_search.albums]
-        artists = [
-            translator.to_artist(sp_artist) for sp_artist in sp_search.artists]
-        tracks = [
-            translator.to_track(sp_track) for sp_track in sp_search.tracks]
-
-        return models.SearchResult(
-            uri=uri, albums=albums, artists=artists, tracks=tracks)
-
-    def _search_by_uri(self, query):
-        tracks = []
-        for uri in query['uri']:
-            tracks += self.lookup(uri)
-
-        uri = 'spotify:search'
-        if len(query['uri']) == 1:
-            uri = query['uri'][0]
-
-        return models.SearchResult(uri=uri, tracks=tracks)
+        return search.search(
+            self._config, self._backend._session,
+            self._backend._online.is_set(),
+            query, uris, exact)
