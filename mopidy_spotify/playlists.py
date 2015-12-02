99d4211177f214ebacd0c6096b58835923b6f35c
Lino Helms <lino@lino.io>
2015-12-02 19:45:35 +0100

    Added playlist saving

--- mopidy_spotify/playlists.py
+++ mopidy_spotify/playlists.py
@@ -90,7 +90,73 @@ class SpotifyPlaylistsProvider(backend.PlaylistsProvider):
         pass  # TODO
 
     def save(self, playlist):
-        pass  # TODO
+        try:
+            sp_playlist = self._backend._session.get_playlist(playlist.uri)
+        except spotify.Error as exc:
+            logger.debug('Failed to lookup Spotify URI %s: %s', uri, exc)
+            return
+        if not sp_playlist.is_loaded:
+            logger.debug(
+                'Waiting for Spotify playlist to load: %s', sp_playlist)
+            sp_playlist.load()
+        if sp_playlist.name != playlist.name:
+            logger.debug("Renaming playlist %s to %s", sp_playlist.name, playlist.name)
+            sp_playlist.name = playlist.name
+        if len(sp_playlist.tracks) < len(playlist.tracks): # item was added
+            added_uri = None
+            added_idx = None
+            for idx, track in enumerate(playlist.tracks):
+                try:
+                    if sp_playlist.tracks[idx].link.uri != track.uri:
+                        added_uri = track.uri
+                        added_idx = idx
+                except IndexError: # item was added to the end
+                    added_uri = track.uri
+                    added_idx = idx
+            try:
+                added_track = self._backend._session.get_track(added_uri)
+                added_track.load()
+                sp_playlist.add_tracks(added_track, added_idx)
+            except spotify.Error as exc:
+                logger.debug('Failed to lookup Spotify URI %s: %s', added_uri, exc)
+                return
+            logger.info('Added track %s to playlist', added_track)
+        elif len(sp_playlist.tracks) > len(playlist.tracks): # item was removed
+            removed_idx = None
+            removed_track = None
+            for idx, track in enumerate(sp_playlist.tracks):
+                try:
+                    if playlist.tracks[idx].uri != track.link.uri:
+                        removed_idx = idx
+                        removed_track = track
+                        break
+                except IndexError: # last item was removed
+                    removed_idx = idx
+            try:
+                sp_playlist.remove_tracks(removed_idx)
+            except spotify.Error as exc:
+                logger.debug('Failed to remove track from playlist %s', exc)
+                return
+            logger.info('Removed track %s from playlist', removed_track)
+        else: # item was reordered
+            source_idx = None
+            target_idx = None
+            changed_uri = None
+            for idx, track in enumerate(playlist.tracks):
+                if sp_playlist.tracks[idx].link.uri != track.uri:
+                    target_idx = idx
+                    changed_uri = track.uri
+                    for spidx, sptrack in enumerate(sp_playlist.tracks):
+                        if sptrack.link.uri == changed_uri:
+                            source_idx = spidx
+                    if not source_idx: #something is wrong
+                        logger.debug('Unable to find source index in assumed reorder operation')
+                        return
+            try:
+                sp_playlist.reorder_tracks(source_idx, target_idx)
+                logger.info('Reordered track %s in playlist', sp_playlist.tracks[source_idx])
+            except spotify.Error as exc:
+                logger.debug('Failed to reorder tracks in playlist %s', exc)
 
 
 def on_container_loaded(sp_playlist_container):
