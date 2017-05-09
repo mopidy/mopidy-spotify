bbf38ee88bcd986ce614692246549ef60e3927b4
Matt Will <mat@butchery.io>
2017-05-09 18:57:42 +0800

    Make load calls respect timeout

--- mopidy_spotify/lookup.py
+++ mopidy_spotify/lookup.py
@@ -45,7 +45,7 @@ def lookup(config, session, uri):
 
 def _lookup_track(config, sp_link):
     sp_track = sp_link.as_track()
-    sp_track.load()
+    sp_track.load(config['timeout'])
     track = translator.to_track(sp_track, bitrate=config['bitrate'])
     if track is not None:
         yield track
@@ -54,7 +54,7 @@ def _lookup_track(config, sp_link):
 def _lookup_album(config, sp_link):
     sp_album = sp_link.as_album()
     sp_album_browser = sp_album.browse()
-    sp_album_browser.load()
+    sp_album_browser.load(config['timeout'])
     for sp_track in sp_album_browser.tracks:
         track = translator.to_track(
             sp_track, bitrate=config['bitrate'])
@@ -66,13 +66,13 @@ def _lookup_artist(config, sp_link):
     sp_artist = sp_link.as_artist()
     sp_artist_browser = sp_artist.browse(
         type=spotify.ArtistBrowserType.NO_TRACKS)
-    sp_artist_browser.load()
+    sp_artist_browser.load(config['timeout'])
 
     # Get all album browsers we need first, so they can start retrieving
     # data in the background.
     sp_album_browsers = []
     for sp_album in sp_artist_browser.albums:
-        sp_album.load()
+        sp_album.load(config['timeout'])
         if not sp_album.is_available:
             continue
         if sp_album.type is spotify.AlbumType.COMPILATION:
@@ -82,7 +82,7 @@ def _lookup_artist(config, sp_link):
         sp_album_browsers.append(sp_album.browse())
 
     for sp_album_browser in sp_album_browsers:
-        sp_album_browser.load()
+        sp_album_browser.load(config['timeout'])
         for sp_track in sp_album_browser.tracks:
             track = translator.to_track(
                 sp_track, bitrate=config['bitrate'])
@@ -92,9 +92,9 @@ def _lookup_artist(config, sp_link):
 
 def _lookup_playlist(config, sp_link):
     sp_playlist = sp_link.as_playlist()
-    sp_playlist.load()
+    sp_playlist.load(config['timeout'])
     for sp_track in sp_playlist.tracks:
-        sp_track.load()
+        sp_track.load(config['timeout'])
         track = translator.to_track(
             sp_track, bitrate=config['bitrate'])
         if track is not None:
--- mopidy_spotify/translator.py
+++ mopidy_spotify/translator.py
@@ -45,9 +45,9 @@ def to_artist_ref(sp_artist):
     return models.Ref.artist(uri=sp_artist.link.uri, name=sp_artist.name)
 
 
-def to_artist_refs(sp_artists):
+def to_artist_refs(sp_artists, timeout=None):
     for sp_artist in sp_artists:
-        sp_artist.load()
+        sp_artist.load(timeout)
         ref = to_artist_ref(sp_artist)
         if ref is not None:
             yield ref
@@ -88,9 +88,9 @@ def to_album_ref(sp_album):
     return models.Ref.album(uri=sp_album.link.uri, name=name)
 
 
-def to_album_refs(sp_albums):
+def to_album_refs(sp_albums, timeout=None):
     for sp_album in sp_albums:
-        sp_album.load()
+        sp_album.load(timeout)
         ref = to_album_ref(sp_album)
         if ref is not None:
             yield ref
@@ -142,9 +142,9 @@ def to_track_ref(sp_track):
     return models.Ref.track(uri=sp_track.link.uri, name=sp_track.name)
 
 
-def to_track_refs(sp_tracks):
+def to_track_refs(sp_tracks, timeout=None):
     for sp_track in sp_tracks:
-        sp_track.load()
+        sp_track.load(timeout)
         ref = to_track_ref(sp_track)
         if ref is not None:
             yield ref
