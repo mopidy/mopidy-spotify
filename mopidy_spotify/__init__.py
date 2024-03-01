1aab6bfa6db7dac0764d79c5993cba7fd73dd337
Stein Magnus Jodal <stein.magnus.jodal@oda.com>
2024-03-01 13:18:19 +0100

    Replace pkg_resources with importlib.metadata

--- mopidy_spotify/__init__.py
+++ mopidy_spotify/__init__.py
@@ -1,9 +1,9 @@
 import pathlib
+from importlib.metadata import version
 
-import pkg_resources
 from mopidy import config, ext
 
-__version__ = pkg_resources.get_distribution("Mopidy-Spotify").version
+__version__ = version("Mopidy-Spotify")
 
 
 class Extension(ext.Extension):
